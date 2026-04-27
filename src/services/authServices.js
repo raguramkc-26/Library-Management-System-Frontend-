354c3d409a3e2a8123c490f4b7050def8f815d82
raguramkc-26 <raguramkc038@gmail.com>
2026-04-27 23:17:56 +0530

    issues done

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,21 +1,9 @@
 import instance from "../instances/instance"
 
-export const registerUser = async (userData) => {
-    const response = await instance.post('/auth/register', userData);
-    return response.data;
-}
+export const registerUser = (userData) => instance.post('/auth/register', userData);
 
-export const loginUser = async (data) => {
-    const response = await instance.post('/auth/login', data);
-    return response.data;
-};
+export const loginUser = (data) => instance.post('/auth/login', data);
 
-export const getMe = async () => {
-    const response = await instance.get('/auth/getMe'); 
-    return response.data;
-};
+export const getMe = () => instance.get('/auth/me'); 
 
-export const logoutUser = async () => {
-    const response = await instance.post('/auth/logout'); 
-    return response.data;
-};
+export const logoutUser = () => instance.post('/auth/logout'); 
