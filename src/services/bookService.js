1de14ac4a261eb71f5da769831946ecd892c8e78
raguramkc-26 <raguramkc038@gmail.com>
2026-05-02 15:06:04 +0530

    done

--- src/services/bookService.js
+++ src/services/bookService.js
@@ -1,17 +1,5 @@
 import api from "./api";
 
-export const getAllBooks = (params) =>
-  api.get("/books", { params });
-
-export const getBookById = (id) =>
-  api.get(`/books/${id}`);
-
-export const createBook = (data) =>
-  api.post("/books", data);
-
-export const updateSingleBook = (id, data) =>
-  api.put(`/books/${id}`, data);
-
 export const getMyBorrowings = () =>
   api.get("/borrow/me");
 
@@ -19,7 +7,7 @@ export const borrowBook = (bookId) =>
   api.post(`/borrow/${bookId}`);
 
 export const reserveBook = (bookId) =>
-  api.post(`/reservation/${bookId}`);
+  api.post(`/borrow/${bookId}/reserve`);
 
 export const returnBook = (borrowId) =>
   api.put(`/borrow/${borrowId}/return`);
